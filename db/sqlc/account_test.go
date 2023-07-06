4246a2eed81725310784959b5c19ba27abb84966
Gokul B <gokul.b@sap.com>
2023-07-06 20:48:56 +0530

    Added unit test for accounts tabel

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,14 +2,16 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/Gokul-B12/simplebank/util"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func CreateRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomBalance(),
@@ -29,5 +31,79 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+
+}
+
+func TestCreateAccount(t *testing.T) {
+	CreateRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := CreateRandomAccount(t)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.Equal(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := CreateRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: util.RandomBalance(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, arg.Balance, account2.Balance)
+}
+
+func TestListAccount(t *testing.T) {
+
+	for i := 0; i < 10; i++ {
+		CreateRandomAccount(t)
+	}
+
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, accounts, int(arg.Limit))
+
+	// there will be 5 accounts, so we are checking for NotEmpty on all 5.
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
+
+func TestDeleteAccount(t *testing.T) {
+
+	account1 := CreateRandomAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Empty(t, account2)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
 
 }
