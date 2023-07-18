519f00dfe613952e8b21968b2f6a48f2b85ea7ab
BGokul_Ubuntu <172-31-84-47>
2023-07-18 07:35:44 +0000

    regen sqlc

--- db/query/account.sql
+++ db/query/account.sql
@@ -28,7 +28,7 @@ WHERE id = $1
 RETURNING *;
 
 -- name: AddAccountBalance :one
-UPDATE accounts SET balance = $balance + sqlc.arg(amount)
+UPDATE accounts SET balance = balance + sqlc.arg(amount)
 WHERE id = sqlc.arg(id)
 RETURNING *;
 
