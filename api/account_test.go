0d0f879a7ed8373b053c5bf5c1066b27e6ba0e91
Gokul B <gokul.b@sap.com>
2023-08-03 20:41:42 +0530

    added TestListAccountAPI without for loop

--- api/account_test.go
+++ api/account_test.go
@@ -117,39 +117,150 @@ func TestCreateAccountAPI(t *testing.T) {
 
 	account := randomAccount()
 
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(mockStore *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"Owner":    account.Owner,
+				"Currency": account.Currency,
+			},
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				arg := db.CreateAccountParams{
+					Owner:    account.Owner,
+					Currency: account.Currency,
+					Balance:  0,
+				}
+
+				mockStore.
+					EXPECT().
+					CreateAccount(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(account, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name: "InvalidData",
+			body: gin.H{
+				"Owner":    "",
+				"Currency": account.Currency,
+			},
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				mockStore.EXPECT().
+					CreateAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			body: gin.H{
+				"Owner":    account.Owner,
+				"Currency": account.Currency,
+			},
+			buildStubs: func(mockStore *mockdb.MockStore) {
+				arg := db.CreateAccountParams{
+					Owner:    account.Owner,
+					Currency: account.Currency,
+					Balance:  0,
+				}
+				mockStore.EXPECT().
+					CreateAccount(gomock.Any(), arg).
+					Times(1).
+					Return(db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockStore := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(mockStore)
+			server := NewServer(mockStore)
+
+			recorder := httptest.NewRecorder()
+
+			url := "/accounts"
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
+
+func TestListAccountsAPI(t *testing.T) {
+
+	n := 5
+	accounts := make([]db.Account, n)
+
+	for i := 0; i < 5; i++ {
+		accounts[i] = randomAccount()
+	}
+
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	mockStore := mockdb.NewMockStore(ctrl)
 
-	arg := db.CreateAccountParams{
-		Owner:    account.Owner,
-		Currency: account.Currency,
-		Balance:  0,
+	type Query struct {
+		pageID   int
+		pageSize int
+	}
+	query := Query{
+		pageID:   1,
+		pageSize: n,
 	}
 
-	mockStore.
-		EXPECT().
-		CreateAccount(gomock.Any(), gomock.Eq(arg)).
+	arg := db.ListAccountsParams{
+		Limit:  int32(n),
+		Offset: 0,
+	}
+
+	mockStore.EXPECT().
+		ListAccounts(gomock.Any(), gomock.Eq(arg)).
 		Times(1).
-		Return(account, nil)
+		Return(accounts, nil)
 
 	server := NewServer(mockStore)
 
 	recorder := httptest.NewRecorder()
 
 	url := "/accounts"
-	body := gin.H{
-		"Owner":    account.Owner,
-		"Currency": account.Currency,
-	}
-	data, err := json.Marshal(body)
-	require.NoError(t, err)
-	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+
+	request, err := http.NewRequest(http.MethodGet, url, nil)
 	require.NoError(t, err)
 
+	// Add query parameters to request URL
+	q := request.URL.Query()
+	q.Add("page_id", fmt.Sprintf("%d", query.pageID))
+	q.Add("page_size", fmt.Sprintf("%d", query.pageSize))
+	request.URL.RawQuery = q.Encode()
+
+	// request, err = http.NewRequest(http.MethodGet, url, nil)
+	// require.NoError(t, err)
 	server.router.ServeHTTP(recorder, request)
-	require.Equal(t, http.StatusOK, recorder.Code)
+
+	require.Equal(t, recorder.Code, http.StatusOK)
 
 }
 
